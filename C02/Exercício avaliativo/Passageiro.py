fba1b916ee87d34f9b427f45b3e22106444a0756
brunoquirino14 <brunoquirino@outlook.com.br>
2024-09-22 11:32:02 -0300

    Atualizando exercício avaliativo

---
+++ C02/Exercício avaliativo/Passageiro.py
@@ -0,0 +1,9 @@
+class Passageiro:
+    def __init__(self, nome: str, documento: str):
+        self.nome = nome
+        self.documento = documento
+    def to_dict(self):
+        return {
+            "nome": self.nome,
+            "documento": self.documento
+        }
