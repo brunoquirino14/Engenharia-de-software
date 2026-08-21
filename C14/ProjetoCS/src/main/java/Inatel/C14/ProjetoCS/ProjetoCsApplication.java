a1c809161b71cad3c481fe780d048faf4691eb48
brunoquirino14 <brunoquirino@outlook.com.br>
2026-08-21 20:18:35 -0300

    Initial commit

---
+++ C14/ProjetoCS/src/main/java/Inatel/C14/ProjetoCS/ProjetoCsApplication.java
@@ -0,0 +1,13 @@
+package Inatel.C14.ProjetoCS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCsApplication.class, args);
+	}
+
+}
---
+++ C14/ProjetoCS/src/test/java/Inatel/C14/ProjetoCS/ProjetoCsApplicationTests.java
@@ -0,0 +1,13 @@
+package Inatel.C14.ProjetoCS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
